b200c6b6740a3508518b517ef101317b09a14adc
Maksim Kozhin <a.zurnaaaa@gmail.com>
2024-02-14 22:18:27 +0300

    Initial commit

---
+++ api_with_restrictions/advertisements/filters.py
@@ -0,0 +1,14 @@
+from django_filters import rest_framework as filters
+from django_filters.filters import DateFromToRangeFilter
+
+from advertisements.models import Advertisement
+
+
+class AdvertisementFilter(filters.FilterSet):
+    """Фильтры для объявлений."""
+    created_at = DateFromToRangeFilter()
+    # TODO: задайте требуемые фильтры
+
+    class Meta:
+        model = Advertisement
+        fields = ['created_at', 'updated_at']
---
+++ api_with_restrictions/advertisements/views.py
@@ -0,0 +1,24 @@
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.throttling import UserRateThrottle, AnonRateThrottle
+
+from .models import Advertisement
+from .serializers import AdvertisementSerializer
+from .filters import AdvertisementFilter
+from .permissions import IsOwnerOrReadOnly
+
+
+class AdvertisementViewSet(ModelViewSet):
+    """ViewSet для объявлений."""
+    queryset = Advertisement.objects.all()
+    serializer_class = AdvertisementSerializer
+    filterset_class = AdvertisementFilter
+    throttle_classes = [UserRateThrottle, AnonRateThrottle]
+    # TODO: настройте ViewSet, укажите атрибуты для кверисета,
+    #   сериализаторов и фильтров
+
+    def get_permissions(self):
+        """Получение прав для действий."""
+        if self.action in ["create", "update", "partial_update"]:
+            return [IsAuthenticated(), IsOwnerOrReadOnly()]
+        return []
